a98e2ef4a09a5edba15208a9980a16433ed009c1
XuanKhanh <xuankhanh379@gmail.com>
2022-04-18 22:47:10 +0700

    update2

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import TodoForm from './component/TodoForm';
 
 
 function TodoFeature(props) {
+    const handleTodoFormSubmit=(values)=>{
+console.log(values);
+    }
     return (
         <div>
-            todo
+            <h3>what to do ?</h3>
+            <TodoForm onSubmit={handleTodoFormSubmit}/>
         </div>
     );
 }
