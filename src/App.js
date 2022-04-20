e6fd01f2750ca8de851c02c0e7cc9f934962fa74
XuanKhanh <xuankhanh379@gmail.com>
2022-04-20 23:20:33 +0700

    update 2004

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import { Button } from "@mui/material";
 import React, { useEffect } from "react";
-import { Link, Route, Switch } from "react-router-dom";
 import productApi from "./api/productApi";
-import NotFound from "./component/NotFound";
-import AlbumFeature from "./features/Album";
-import TodoFeature from "./features/Todo/index";
+import Header from "./component/Header";
 
 function App() {
   useEffect(() => {
     const fetchProducts = async () => {
-      const params={
-        _limit:10,
+      const params = {
+        _limit: 10,
       }
-const productList=await productApi.getAll(params)
-console.log(productList);
+      const productList = await productApi.getAll(params)
+      console.log(productList);
     }
     fetchProducts();
   }, [])
   return (
     <div className="App">
-
+      {/* 
       <p>
         <Link to='/todo'>Todo</Link>
       </p>
       <Button variant="contained" color="success" size="medium"><Link to="/album">Album</Link> </Button>
-
       <Switch>
 
         <Route path='/todo' component={TodoFeature} />
         <Route path="/album" component={AlbumFeature} />
         <Route component={NotFound} />
-      </Switch>
+      </Switch> */}
+      <Header />
 
     </div>
   );
--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.scss'
+
 
 Header.propTypes = {
-    
+
 };
 
 function Header(props) {
     return (
-        <div>
-            Header
+        <div className='header'>
+            <div className='header__left'>
+                <div className='header__left--content'>
+                    <div className='header__left--logo'>
+                        <i class="fa-brands fa-facebook"></i>
+                    </div>
+
+                    <div className='header__left--search'>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div className='header__center'>
+                <div>
+                    <ul className='header__center--navbar'>
+                        <li>
+                            <i class="fa-solid fa-house"></i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-youtube"></i>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-store"></i>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-users-line"></i>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-gamepad"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='header__right'>
+                <div className='header__right--info'>
+                    <span><i class="fa-brands fa-facebook-messenger"></i></span>
+                    <span><i class="fa-solid fa-bell"></i></span>
+                    <span><i class="fa-solid fa-caret-down"></i></span>
+                </div>
+            </div>
         </div>
     );
 }
