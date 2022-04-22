89a751bc7288a62872d74dc68b407d56694ce688
xuankhanh379 <xuankhanh379@gmail.com>
2022-04-22 16:48:51 +0700

    update 2204

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React, { useEffect } from "react";
-import { Link, Route, Switch } from "react-router-dom";
-import productApi from "./api/productApi";
-import AlbumFeature from "./features/Album";
-import Header from "./component/Header";
-import NotFound from "./component/NotFound";
-import TodoFeature from "./features/Todo";
-import { Button } from "@mui/material";
-import Content from "./features/Content";
-
+import React, { useEffect } from 'react';
+import productApi from './api/productApi';
+import Header from './component/Header';
+import Content from './features/Content';
 
 function App() {
   useEffect(() => {
     const fetchProducts = async () => {
       const params = {
         _limit: 10,
-      }
-      const productList = await productApi.getAll(params)
+      };
+      const productList = await productApi.getAll(params);
       console.log(productList);
-    }
+    };
     fetchProducts();
-  }, [])
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Content />
-
-
-
     </div>
   );
 }
